da02b15a34871d0c30db2c5c47a4449789c1b46b
ikkiko <mushroomkik@gmail.com>
2024-08-10 16:16:37 +0700

    update post books

--- main.py
+++ main.py
@@ -8,7 +8,25 @@ FileBook = open('books.json')
 
 BookJson = json.load(FileBook)
 
+class Post(BaseModel):
+    author: str
+    country: str
+    imageLink: str
+    language: str
+    link: str
+    pages: int
+    title: str
+    year: int
+
 #Get all books
 @app.get("/books")
 def get_all_book():
-    return {"data": BookJson}
+    return {"Books": BookJson}
+
+#Post a new post(Create)
+@app.post("/books", status_code =status.HTTP_201_CREATED)
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict["pages"] = len(BookJson) + 1
+    BookJson.append(post_dict)
+    return{"Books": BookJson}
