55c7282a9c486956112c45624ce7a0c6f71657fa
ikkiko <mushroomkik@gmail.com>
2024-09-16 22:52:25 +0700

    Add post book store
    Add post book store

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-FileBook = open('books.json')
+with open("books.json", "r") as FileBook:
+    BookJson = json.load(FileBook)
 
-BookJson = json.load(FileBook)
+with open("textbook.json", "r") as TextBook:
+    TextJson = json.load(TextBook)
+
+#FileBook = open('books.json')
+#BookJson = json.load(FileBook)
 
 class Post(BaseModel):
     author: str
@@ -18,15 +23,46 @@ class Post(BaseModel):
     title: str
     year: int
 
+TestBook = {
+    "author": "Butterbear",
+    "country": "Thailand",
+    "imageLink": "butterbear.co.th",
+    "language": "Thai",
+    "link": "NongNeay.com",
+    "pages": 3,
+    "title": "NoeyNoi School",
+    "year": 1993
+}
+
 #Get all books
 @app.get("/books")
 def get_all_book():
     return {"Books": BookJson}
 
+
 #Post a new post(Create)
 @app.post("/books", status_code =status.HTTP_201_CREATED)
 def create_post(post: Post):
     post_dict = post.dict()
-    post_dict["pages"] = len(BookJson) + 1
+    print(post_dict)
+    print(type(post_dict))
+#   post_dict["pages"] = len(BookJson) + 1
     BookJson.append(post_dict)
+    print(BookJson)
+#    BookJson.update(post_dict)
     return{"Books": BookJson}
+    
+
+#with open('books.json' , 'w') as FileBook:
+ #   json.dumps(BookJson,FileBook)
+
+if 0:
+    with open("books.json", "w") as FileBook:
+        json.dump(BookJson, FileBook, indent=4)
+        #FileBook.write(json.dump(BookJson, FileBook, indent=4))
+
+if 1:
+    TextJson.append(TestBook)
+    with open("textbook.json", "w") as TextBook:
+        json.dump(TextJson, TextBook, indent=4)
+        #TextBook.write(json.dump(TextJson, TextBook, indent=4))    
